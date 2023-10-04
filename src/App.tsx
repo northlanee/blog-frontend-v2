d2147f4ae2e3bf57582431bb35d8e0a65f4e6966
northlaneee <alex.yokai00@gmail.com>
2023-10-04 17:28:05 +0300

    router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
-import { Test } from "./Test";
+import { Route, Routes } from "react-router";
+import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy";
+import { MainPageLazy } from "./pages/MainPage/MainPage.lazy";
+import { Link } from "react-router-dom";
+import { Suspense } from "react";
 
 export const App = () => {
-  return <Test />;
+  return (
+    <div className="app">
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/about" element={<AboutPageLazy />} />
+          <Route path="/" element={<MainPageLazy />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
 };
--- src/Test.tsx
+++
@@ -1,5 +0,0 @@
-import styles from "./Test.module.scss";
-
-export const Test = () => {
-  return <div className={styles.back}>1111</div>;
-};
